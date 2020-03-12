183948eaf71b2fe4ce7d66508a20ccb397cae7a1
liuye <ye.liu01@hand-china.com>
2020-03-12 18:10:53 +0800

    优化目录结构

--- startup.sh
+++ startup.sh
@@ -2,6 +2,11 @@
 
 PID=`ps -ef|grep 'python3 -m http.server --cgi'|grep -v 'grep' | awk '{print $2}'`
 if [ -z $PID ];then
+    cd www
+    if [ ! -d files ];then
+        mkdir files
+        echo 'files dir is created!'
+    fi
     nohup python3 -m http.server --cgi > /dev/null 2>&1 &
     echo 'file server started!'
 else
---
+++ www/cgi-bin/uploads.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# coding=utf-8
+submit_url = 'http://localhost:8000/cgi-bin/server.py'
+html_content = '''
+<html>
+<head>
+    <meta http-equiv="content-type" content="txt/html; charset=utf-8">
+    <title>file upload</title>
+</head>
+<style>
+    .box {
+        width: 300px;
+        height: 120px;
+        border: 2px solid yellowgreen;
+        position: absolute;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        margin: auto;
+    }
+</style>
+
+<body>
+
+    <div>
+        <form action="submit_url" method="post" enctype="multipart/form-data">
+            <table class="box">
+                <tr>
+                    <th><label for="file">文件:</label></th>
+                    <th><input name="uploadfile" id="file" type="file"/></th>
+                </tr>
+                <tr>
+                    <th><label for="password">口令:</label></th>
+                    <th><input name="password" id="password" type="password"/></th>
+                </tr>
+                <tr>
+                    <th colspan="2"><input type="submit" name="submit" value="提交"/></th>
+                </tr>
+            </table>
+        </form>
+    </div>
+
+</body>
+
+</html>
+'''.replace('submit_url',submit_url)
+print(html_content)
+
